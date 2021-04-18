f428077e21711125697b0e4fcd8a06108cf2941b
Sera Brocious <sera.brocious@gmail.com>
2021-04-18 13:29:02 -0400

    Massively cleaned up Python backend output.

--- backends/python.py
+++ backends/python.py
@@ -3,6 +3,29 @@ from backend import banner, Backend
 import keyword
 import struct as S
 
+headerCode = r'''
+import struct as __struct__
+def _arrayUnpack(fp, fmt, elemSize, rank):
+	return list(__struct__.unpack('<' + fmt*rank, fp.read(elemSize*rank)))
+def _prepr(expr, indent=1):
+	if isinstance(expr, list) or isinstance(expr, tuple):
+		cc = '[%s]' if isinstance(expr, list) else '(%s)'
+		elems = [_prepr(x, 2) for x in expr]
+		js = u',\n' if any('\n' in x for x in elems) else u', '
+		if len(elems) == 1 and cc[0] == '(':
+			elems.append('')
+		return cc % js.join(elems).lstrip()
+	expr = repr(expr)
+	if '\n' in expr:
+		ti = '\t' * indent
+		expr = expr.split('\n')
+		if indent == 1:
+			return u'\n'.join([expr[0]] + [ti + x for x in expr[1:]])
+		else:
+			return u'\n'.join(ti + x for x in expr)
+	return expr
+'''.strip()
+
 def sanitize(name):
 	if name in keyword.kwlist or name[0] in '0123456789':
 		return sanitize('_' + name)
@@ -55,18 +78,24 @@ class PythonBackend(Backend):
 		self.writeLine('#')
 		self.writeLine('# DO NOT EDIT')
 		self.writeLine('# Generated automatically by Fluffy')
-		self.writeLine('import struct, pprint')
+
+		self.writeLine(headerCode)
 
 		for name, struct in spec.structs.items():
 			self.writeLine()
-			name = name.replace('<', '_').replace('>', '')
 			self.writeLine('class %s(object):' % sanitize(name))
+			self.indent()
+			self.writeLine('def __init__(self%s):' % (', ' + u', '.join(sanitize(fn) + '=None' for fn in struct.fields) if struct.fields else ''))
 			self.indent()
 			if struct.fields:
 				for fn, fts in struct.fields.items():
-					self.writeLine('%s = None # %s' % (sanitize(fn), u' or '.join(map(repr, fts))))
-				self.writeLine()
-			self.writeLine('def __init__(self, fp%s):' % (', ' + u', '.join(map(sanitize, sorted(struct.dependencies.keys()))) if struct.dependencies else ''))
+					self.writeLine('self.%s = %s # %s' % (sanitize(fn), sanitize(fn), u' or '.join(map(repr, fts))))
+			else:
+				self.writeLine('pass')
+			self.dedent()
+			self.writeLine()
+
+			self.writeLine('def __unpack__(self, fp%s):' % (', ' + u', '.join(map(sanitize, sorted(struct.dependencies.keys()))) if struct.dependencies else ''))
 			self.indent()
 			def recur(steps):
 				for step in steps:
@@ -113,6 +142,7 @@ class PythonBackend(Backend):
 						print step
 						assert False
 			recur(struct.unpackSteps)
+			self.writeLine('return self')
 			self.dedent()
 
 			self.writeLine()
@@ -122,21 +152,18 @@ class PythonBackend(Backend):
 			if len(toPrint) == 0:
 				self.writeLine('return \'%s()\'' % sanitize(name))
 			else:
-				self.writeLine('return ("""%s(' % sanitize(name))
-				prev = self.curIndent
-				self.curIndent = 0
-				for i, fn in enumerate(toPrint):
-					self.writeLine('  %s=%%s%s' % (fn, ',' if i < len(toPrint) - 1 else ''))
-				self.writeLine(')"""')
-				self.curIndent = prev
-				self.writeLine('% (')
-				self.indent()
-				for i, fn in enumerate(toPrint):
-					self.writeLine('(lambda lines: u"\\n".join([lines[0]] + ["  " + x for x in lines[1:]]))(pprint.pformat(self.%s).split(u"\\n"))%s' % (fn, ',' if i < len(toPrint) - 1 else ''))
-				self.dedent()
-				self.writeLine('))')
+				self.writeLine('ret = \'%s(\\n\'' % sanitize(name))
+				for fn in toPrint:
+					self.writeLine('if self.%s is not None:' % fn)
+					self.indent()
+					self.writeLine('ret += \'\\t%s=%%s,\\n\' %% _prepr(self.%s)' % (fn, fn))
+					self.dedent()
+				self.writeLine('return ret + \')\'')
 			self.dedent()
 			self.dedent()
+
+		self.writeLine()
+		self.writeLine('__all__ = %r' % map(sanitize, spec.structs.keys()))
 
 	def genUnpack(self, type, struct, count):
 		mult = lambda x: x if count == 1 else u', '.join([x] * count)
@@ -146,18 +173,19 @@ class PythonBackend(Backend):
 			else:
 				fc = ffc[type.bits]
 			fmt = '<' + fc * count
-			cmd = 'struct.unpack(%r, fp.read(%i))' % (fmt, S.calcsize(fmt))
+			cmd = '__struct__.unpack(%r, fp.read(%i))' % (fmt, S.calcsize(fmt))
 			return cmd + '[0]' if count == 1 else cmd
 		elif isinstance(type, ArrayType):
 			bt = type.base
 			while isinstance(bt, Typedef):
 				bt = bt.otype
-			rank = genExpr(type.rankExpr, struct)
 			try:
-				irank = int(rank)
-				return mult(self.genUnpack(bt, struct, irank))
+				if type.rankExpr[0] == 'value':
+					irank = int(type.rankExpr[1])
+					return mult(self.genUnpack(bt, struct, irank))
 			except:
 				pass
+			rank = genExpr(type.rankExpr, struct)
 			
 			if isinstance(bt, IntType) or isinstance(bt, FloatType):
 				if isinstance(bt, IntType):
@@ -166,7 +194,7 @@ class PythonBackend(Backend):
 					fc = ifc[(bt.bits, bt.signed)]
 				else:
 					fc = ffc[bt.bits]
-				return mult('list((lambda __rank__: struct.unpack(\'<\' + %r*__rank__, %i*__rank__))(%s))' % (fc, S.calcsize(fc), rank))
+				return mult('_arrayUnpack(fp, %r, %i, %s)' % (fc, S.calcsize(fc), rank))
 			return mult('[%s for _ in xrange(%s)]' % (self.genUnpack(bt, struct, 1), rank))
 		elif isinstance(type, Typedef):
 			return self.genUnpack(type.otype, struct, count)
@@ -175,6 +203,6 @@ class PythonBackend(Backend):
 			depMatch = ''
 			if ts.dependencies:
 				depMatch = ', ' + u', '.join(sanitize(key if key in struct.dependencies else 'self.' + key) for key in sorted(ts.dependencies.keys()))
-			return mult('%s(fp%s)' % (sanitize(type.name), depMatch))
+			return mult('%s().__unpack__(fp%s)' % (sanitize(type.name), depMatch))
 		print '%r %s[%i]' % (type.__class__, type, count)
 		assert False
